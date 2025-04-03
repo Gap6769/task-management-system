2ac99c9e2f1f4b45431de8ce16353172b0d28e65
Hi I'm Gap <90163336+Gap6769@users.noreply.github.com>
2025-04-03 16:46:34 -0300

    implement history of changes for tasks

--- backend/app/routes/tasks.py
+++ backend/app/routes/tasks.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, HTTPException, Depends, Body
 from bson import ObjectId
-from app.models import Task, TaskStatus, TaskUpdate
+from app.models import Task, TaskStatus, TaskUpdate, StatusChange
 from app.database import tasks_collection
 from app.auth import oauth2_scheme
+from datetime import datetime
 
 router = APIRouter()
 
@@ -16,25 +17,39 @@ async def get_tasks():
 
 @router.post("/tasks")
 async def create_task(task: Task, token: str = Depends(oauth2_scheme)):
-    task_dict = task.dict(exclude_unset=True)  # Cambiado a dict()
+    task_dict = task.dict(exclude_unset=True)
     task_dict["status"] = task_dict["status"].value
+    task_dict["status_history"] = [
+        {"status": task_dict["status"], "timestamp": datetime.utcnow()}
+    ]
     result = tasks_collection.insert_one(task_dict)
     task.id = str(result.inserted_id)
     return task
 
 @router.put("/tasks/{task_id}")
 async def update_task(task_id: str, task: TaskUpdate):
+    current_task = tasks_collection.find_one({"_id": ObjectId(task_id)})
+    if not current_task:
+        raise HTTPException(status_code=404, detail="Task not found")
+
+    task_dict = task.dict()
+    if current_task["status"] != task.status.value:
+        task_dict["last_status_change"] = datetime.utcnow()
+        task_dict["status_history"] = current_task.get("status_history", [])
+        task_dict["status_history"].append(
+            {"status": task.status.value, "timestamp": task_dict["last_status_change"]}
+        )
+
     result = tasks_collection.update_one(
         {"_id": ObjectId(task_id)},
-        {"$set": task.dict()}  
+        {"$set": task_dict}
     )
     if result.matched_count == 0:
         raise HTTPException(status_code=404, detail="Task not found")
 
     updated_task = tasks_collection.find_one({"_id": ObjectId(task_id)})
-    
-    updated_task["id"] = str(updated_task["_id"])  # Convertimos el ObjectId a cadena
-    del updated_task["_id"]  # Eliminamos el campo _id
+    updated_task["id"] = str(updated_task["_id"])
+    del updated_task["_id"]
 
     return updated_task
 
