def031eff2422d4680279a22d14deb1fac148653
Hi I'm Gap <90163336+Gap6769@users.noreply.github.com>
2025-04-03 23:28:24 -0300

    ok

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -7,12 +7,12 @@ const API_URL = "https://qjet49m264.execute-api.sa-east-1.amazonaws.com";
 
 export const registerUser = (userData) => axios.post(`${API_URL}/auth/register`, userData);
 export const loginUser = (credentials) => axios.post(`${API_URL}/auth/token`, credentials);
-export const getTasks = (token) => axios.get(`${API_URL}/tasks`, { headers: { Authorization: `Bearer ${token}` } });
-export const createTask = (taskData, token) => axios.post(`${API_URL}/tasks`, taskData, { headers: { Authorization: `Bearer ${token}` } });
+export const getTasks = (token) => axios.get(`${API_URL}/api/tasks`, { headers: { Authorization: `Bearer ${token}` } });
+export const createTask = (taskData, token) => axios.post(`${API_URL}/api/tasks`, taskData, { headers: { Authorization: `Bearer ${token}` } });
 export const updateTask = (taskId, taskData, token) => {
-    return axios.put(`${API_URL}/tasks/${taskId}`, taskData, { 
+    return axios.put(`${API_URL}/api/tasks/${taskId}`, taskData, { 
       headers: { Authorization: `Bearer ${token}` } 
     });
   };
   
-export const deleteTask = (taskId, token) => axios.delete(`${API_URL}/tasks/${taskId}`, { headers: { Authorization: `Bearer ${token}` } });
+export const deleteTask = (taskId, token) => axios.delete(`${API_URL}/api/tasks/${taskId}`, { headers: { Authorization: `Bearer ${token}` } });
